f6596f2da66b62b3e12e9ec26aac855228973979
Alexander Tigselema <alexxtp@gmail.com>
2024-09-23 19:44:13 -0500

    Navigate to porfolio

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div className="Header flex items-center p-6 w-full">
-      <h1 className="text-2xl font-bold">Atpsito</h1>
+      <a href="https://atpsito.com/#code">
+        <h1 className="text-2xl font-bold">Atpsito</h1>
+      </a>
     </div>
   );
 };
